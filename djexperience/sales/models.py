163bbfb227d720aa66aac0bcdb1239265700dfa7
rg3915 <rg3915@yahoo.com.br>
2016-05-18 15:59:52 -0300

    Ordered: ForeignKey

--- djexperience/sales/models.py
+++ djexperience/sales/models.py
@@ -22,3 +22,9 @@ class Sale(models.Model):
     method_paid = models.CharField(
         'forma da pagamento', max_length=2, choices=METHOD_PAID)
     deadline = models.CharField('prazo de entrega', max_length=50)
+
+
+class Ordered(models.Model):
+    customer = models.ForeignKey('Customer', verbose_name='cliente')
+    status_ordered = models.CharField(
+        'status', max_length=2, choices=STATUS_LIST)
